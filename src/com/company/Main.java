f0405df1deeaac727a3470f0898020ad1101e685
tiw <tam3393@hotmail.co.th>
2017-11-08 19:00:01 +0700

    system in

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,37 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
-//    public static void main(String[] args) {
+    String Label[];
+    public static void main(String[] args) {
 //	// write your code here
-//        System.out.println("test");
-//    }
+        //System.out.println("test");
 
 
 
+    }
 
+    public static void x()  {
+
+
+    Scanner cin = new Scanner(System.in);
+
+
+        System.out.println("Please input command: \t");
+    String inputLabel = cin.next();
+
+    //Display
+        System.out.println("This is your command: ");
+        System.out.println(inputLabel);
 }
+    public static void y() {
+    //if(Label[1])
+
+    }
+}
+
+
+
+
+
+
