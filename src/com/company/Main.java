55aa4f4d222aa7fb00330297cbc781edbf8dc104
apboomap <apboomap@outlook.com>
2017-11-08 18:51:52 +0700

    test fuction line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,16 @@ package com.company;
 
 public class Main {
 
-//    public static void main(String[] args) {
-//	// write your code here
-//        System.out.println("test");
-//    }
+    public static void main(String[] args) {
+	// write your code here
+        String b = "   label  Ins   RA   RB  RD   com";
+        String[] label = b.split("\\s+");
+        if (label[0] == " "){
+            System.out.println("NULL");
+        }else {
+            System.out.println(label[0] + "11");
+        }
+    }
 
 
 
