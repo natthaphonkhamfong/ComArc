a0eb62824568d4e2a8bddc59c6cfd785c41de083
apboomap <apboomap@outlook.com>
2017-11-08 17:51:08 +0700

    feature/add Assembler

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("test");
-    }
+//    public static void main(String[] args) {
+//	// write your code here
+//        System.out.println("test");
+//    }
 
 
 
