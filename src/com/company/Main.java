825f00d18ba8f5489e2cdb6e380220b359d3bf65
apboomap <apboomap@outlook.com>
2017-11-08 19:15:35 +0700

    test1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String b = "   label  Ins   RA   RB  RD   com";
+        String b = "label  Ins   RA   RB  RD   com";
         String[] label = b.split("\\s+");
-        if (label[0] == " "){
-            System.out.println("NULL");
+        if (label[0].isEmpty()){
+            //intstruction(label[1], label[2], label[3] , label[4]);
         }else {
-            System.out.println(label[0] + "11");
+
+            System.out.println(label[0]);
+            //intstruction(label[1], label[2], label[3] , label[4]);
+
         }
     }
 
