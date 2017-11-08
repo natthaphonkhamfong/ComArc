a37e0f59af348fc0da1158d9f69b35ad8ef0d4d8
tiw <tam3393@hotmail.co.th>
2017-11-08 19:24:28 +0700

    test2

--- src/com/company/Assembler.java
+++ src/com/company/Assembler.java
@@ -4,9 +4,20 @@ public class Assembler {
 
     int l = 0; // number line start at 0
 
+
     public void line(String line){
         String[] label = line.split("\\s+");
+
+
         if (label[1] == null);
+
+    }
+    public void instruct(String line){
+
+
+    }
+    public void Opcode(String line){
+
 
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String b = "   label  Ins   RA   RB  RD   com";
-        String[] label = b.split("\\s+");
-        if (label[0] == " "){
-            System.out.println("NULL");
-        }else {
-            System.out.println(label[0] + "11");
-        }
+
+
+
+        Assembler a = new Assembler();
+        a.line(b);
+
     }
 
 
