2b8cd63ff6eeb4e01be7344c096a06d44f1fb9c9
Nattawut Khamchai <nattawut.khamchai@gmail.com>
2017-11-15 14:05:35 +0700

    Assembler v 0.1

--- src/com/company/Assembler.java
+++ src/com/company/Assembler.java
@@ -1,24 +1,90 @@
 package com.company;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
 
 public class Assembler {
 
-    int l = 0; // number line start at 0
+    int l = 0; // number of line start at 0
 
-    public void line(String line){
-        String[] label = line.split("\\s+");
-        if (label[0].isEmpty()){
-            //intstruction(label[1], label[2], label[3] , label[4]);
-        }else {
+    private String inst, rs, rt, rd;
+    private ArrayList<String> label = new ArrayList<String>();
+    private int  regA;
+    private int  regB;
+    private int  destReg;
+    private static boolean isRun;
 
-            System.out.println(label[0]);
-            //intstruction(label[1], label[2], label[3] , label[4]);
 
+    public static void main(String[] args) {
+        isRun = true;
+        Assembler();
+    }
+
+    public void read(String line){
+        String[] CurrentLine = line.split("\\s+");
+        int checklabel = 0;
+        if (!CurrentLine[0].isEmpty()){
+            checklabel = label.indexOf(CurrentLine[0]);
+            if (checklabel > 0){
+                isRun = false;
+                System.out.println("error label already have " + CurrentLine[0]);
+            }else{
+                label.add(CurrentLine[0]);
+            }
+
+        }
+        setInst(CurrentLine[1], CurrentLine[2], CurrentLine[3], CurrentLine[4]);
+
+
+    }
+
+    public static void Assembler(){
+        while (isRun){
+
+        }
+    }
+
+    public void setInst(String inst, String field0 ,String field1 ,String field2) {
+        String binary = "";
+        if(inst.equals("add")){
+            binary = "000";
+        }
+
+        else if (inst.equals("nand")){
+            binary = "001";
+        }
+
+        else if (inst == "lw"){
+            binary = "010";
+        }
+
+        else if (inst.equals("sw")){
+            binary = "011";
+        }
+        else if (inst.equals("beq")){
+            binary = "100";
+        }
+
+        else if (inst.equals("jalr")){
+            binary = "101";
+        }
+
+        else if (inst.equals("halt")){
+            binary = "110";
+        }
+
+        else if (inst.equals("noop")){
+            binary = "000";
         }
 
     }
 
-    public void intstruction(String ints ,String rs ,String rt ,String rd){
-        //code
-     }
+    public void Rtype(String op, int rs, int rt, int rd ){
+        int instruction =
+
+    }
+
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,34 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        String b = "   label  Ins   RA   RB  RD   com";
 
-
-
-        Assembler a = new Assembler();
-        a.line(b);
-
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please insert the filename =");
+        System.out.println("Example = [textfilename].txt where inside is the assembly program.");
+        maindua(s.next());
     }
-
-    zdfgdfgdfgdfgdfgdfgdfgdfgdfg
-
+    public static void main3(){
+        String program=ReadFile.loadFile("assembly.txt");
+        int index=0;
+        ArrayList<String> result = new ArrayList<String>();
+        for (String temp:program.split("\n")){
+            if(temp.length() >1){
+                if(index<10){
+                    result.add("[0"+index+"] "+temp);
+                }
+                else {
+                    result.add("["+index+"] "+temp);
+                }
+                index+=4;
+            }
+        }
+        for(String i : result){
+            System.out.println(i+";");
+        }
+    }
 
 
 }
-it
