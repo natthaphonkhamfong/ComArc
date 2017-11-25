64b03d77123f91a33b963997d704e48d21ae8126
Nattawut Khamchai <nattawut.khamchai@gmail.com>
2017-11-25 12:01:22 +0700

    aof

--- src/com/company/Assembler.java
+++ src/com/company/Assembler.java
@@ -15,12 +15,18 @@ public class Assembler {
     static ArrayList<String> label = new ArrayList<String>();
     static ArrayList<Integer> valueOflabel = new ArrayList<Integer>();
     static ArrayList<String> memory = new ArrayList<String>();
+
     private static boolean isRun, isbeq;
     private static int pc, result;
-    static String pattern = "^[\\-]?\\d*$";
+
+    static String pattern = ("^[\\-]?\\d*$");  //number pattern
+    static String patternAlphabet = ("^[A-Za-z]*"); // alphabet pattern
+
     static BufferedWriter bw = null;
     static FileWriter fw = null;
     static PrintWriter pw = null;
+    static String[] LabelPattern = {"start", "done", "five", "neg1", "stAddr"};
+    static String[] OpcodePattern = {"add", "nand", "lw", "sw", "beq", "jarl", "halt", "noop", ".fill"};  //Opcode Pattern for this PC
 
 
     public static void main(String[] args) throws IOException {
@@ -40,7 +46,7 @@ public class Assembler {
         }
     }
 
-    public static void write(String Currentline) throws IOException { // เขียนแต่ละบรรทัดลงไฟล์ .txt
+    public static void write(String Currentline) throws IOException {       // เขียนแต่ละบรรทัดลงไฟล์ .txt
         try {
             fw = new FileWriter(FileWrite, true);
             bw = new BufferedWriter(fw);
@@ -58,11 +64,11 @@ public class Assembler {
         }
     }
 
-    public static void read(String file) { // อ่าน file
+    public static void read(String file) {                              // อ่าน file assembly.txt
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String Currentline;
             while ((Currentline = br.readLine()) != null) {
-                memory.add(Currentline); // add instruction to memory
+                memory.add(Currentline);                                // add instruction to memory
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,6 +76,7 @@ public class Assembler {
     }
 
     public static void Assembler() throws IOException {  //เริ่มแปลง assembly เป็น machine language
+
         checklabel();
         for (int i = 0; i < memory.size(); i++) {
             //if (checkIsLabel == false) break;
@@ -79,12 +86,15 @@ public class Assembler {
             String[] field = CurrentLine.split("\\s+"); // split white space
             pc++;
 
-            if (field[1].equals(".fill")) {
+            if (field[1].equals(".fill")) {             //ถ้า opcode เป็น fill ใช้ฟังก์ชัน setFill
+
                 setFill(field[2]);
-            } else if (field[1].equals("noop") || field[1].equals("halt")) { //ถ้าเป็น halt หรือ noop ใช้ฟังก์ชัน setOtype();
+            }
+
+            if (field[1].equals("noop") || field[1].equals("halt")) { //ถ้าเป็น halt หรือ noop ใช้ฟังก์ชัน setOtype
                 setOtype(field[1]);
             } else {
-                setInst(field[1]);
+                setInst(field[1]);                      // คำสั่ง opcode ตาม Table1 ใช้ฟังก์ชัน setInst
             }
 
             print();
@@ -92,10 +102,8 @@ public class Assembler {
         }
     }
 
+
     private static void checklabel() {
-        String[] LabelPattern = {"start", "done", "five", "neg1", "stAddr"};
-
-
         for (int i = 0; i < memory.size(); i++) {
             String CurrentLine = memory.get(i);
             String[] field = CurrentLine.split("\\s+");      // split white space
@@ -106,27 +114,40 @@ public class Assembler {
                     isRun = false;
                 }
 
-                if (field[1].equals("fill")) {                // check is Fill?
-                    label.add(field[0]);
+                if (!Arrays.asList(LabelPattern).contains(field[0])) {
+                    System.out.println("label does not exist ---> " + field[0]); //เช็คว่า Label defined?
+                    isRun = false;
+                }
 
-                    if (Pattern.matches(pattern, field[2])) { //Check field2 is Number or Symbol?
-                        valueOflabel.add(convertStritoInt(field[2]));
+
+                if (field[1].equals(".fill")) {                // check is Fill?
+
+                    label.add(field[0]);                      // เก็บ field[0] ไว้ใน ArrayList label
+                    valueOflabel.add(i);
+
+                    if (Pattern.matches(patternAlphabet, field[2]) && !Arrays.asList(LabelPattern).contains(field[2])) {
+                        System.out.println("label is not correct ---> " + field[2]); //เช็คว่า Is it correct label?
+                        isRun = false;
+                    }
+
+                    if (Pattern.matches(pattern, field[2])) {     //Check field2 is Number ?
+                        valueOflabel.add(convertStritoInt(field[2]));   //แปลง field[2] จาก String เป็น integer แล้วเก็บไว้ใน ArrayList ValueOflabel
                     } else {
                         for (int j = 0; j < label.size(); j++) {
                             if (label.get(j).equals(field[2])) {
-                                valueOflabel.add(valueOflabel.get(j));
-                            } else if (!Arrays.asList(LabelPattern).contains(field[0])) {
-                                System.out.println("label does not exist+ " + field[2]); //เช็คว่า Label defined?
-                                isRun = false;
+                                valueOflabel.add(valueOflabel.get(j));  //เก็บค่า address ของ field[2] ไว้ใน ArrayLast ชื่อ valueOflabel
                             }
                         }
                     }
+
+
                 } else {
                     label.add(field[0]);
-                    valueOflabel.add(i);
+                    valueOflabel.add(i);                       //เก็บ Address ที่ Label อยู่
                 }
+
                 int count = 0;
-                for (int j = 0; j < label.size(); j++) {
+                for (int j = 0; j < label.size(); j++) {       //check label ซ้ำ
                     count = 0;
                     for (int k = 0; k < label.size(); k++)
                         if (label.get(j).equals(label.get(k)))
@@ -134,7 +155,7 @@ public class Assembler {
                 }
 
                 if (count >= 2) {
-                    System.out.println(field[0] + "  label already has.");    //Check label does exists
+                    System.out.println("Label " + field[0] + " already has.");    //Check label does exists
                     isRun = false;
                 }
             }
@@ -142,9 +163,10 @@ public class Assembler {
     }
 
 
-    private static void setFill(String field) {
-        if (Pattern.matches(pattern, field)) {
+    private static void setFill(String field) {        //สำหรับคำสั่ง .fill
+        if (Pattern.matches(pattern, field)) {         //
             result = convertStritoInt(field);
+
         } else {
             for (int i = 0; i < label.size(); i++) {
                 if (field.equals(label.get(i))) {
@@ -157,7 +179,7 @@ public class Assembler {
     public static void setInst(String inst) {              //Convert each instruction to opcode
         String CurrentLine = memory.get(pc - 1);
         String[] field = CurrentLine.split("\\s+");         // split white space
-        String[] OpcodePattern = {"add", "nand", "lw", "sw", "beq", "jarl", "halt", "noop", ".fill"};  //Opcode Pattern for this PC
+
 
         if (inst.equals("add")) {
             Rtype("000", field[2], field[3], field[4]);
@@ -201,7 +223,7 @@ public class Assembler {
         int temp = 0;
         String regA = setReg(field0); //rs
         String regB = setReg(field1); //rt
-        String offsetField = "";
+        String offsetField ="";
         if (isbeq) {
             if (Pattern.matches(pattern, field2)) {
                 offsetField = setOffsetField(field2);
@@ -210,11 +232,13 @@ public class Assembler {
                 temp = temp - pc;
                 offsetField = setOffsetField(Integer.toString(temp));
             }
-        } else {
+        }
+        else {
             offsetField = setOffsetField(field2);
         }
 
         String binary = opcpde + regA + regB + offsetField;
+       // System.out.println(binary);
         result = Integer.parseInt(binary, 2);
     }
 
@@ -230,57 +254,63 @@ public class Assembler {
         result = Integer.parseInt(binary, 2);
     }
 
-    private static void print() throws IOException {
-        write(Integer.toString(result));
-        System.out.println(result);
-    }
-
-    private static int convertStritoInt(String field) { // convert String to int
-        return Integer.parseInt(field); // return convert int
-    }
-
-    private static int setVariable(String field) {
-        for (int i = 0; i < label.size(); i++)
-            if (field.equals(label.get(i))) {
-                return valueOflabel.get(i); // โหลดค่าจาก label
-            }
-
-        //not find in label
-        System.out.println("does not exist variable " + field);
-        isRun = false;
-        return 0;
-    }
-
-
-    private static String setOffsetField(String field) {
-        int test = 0;
-        if (Pattern.matches(pattern, field)) {
-            test = Integer.valueOf(field);
-        } else {
-            test = setVariable(field);
-        }
-        if (test > 32767 || test < -32768) {                            //Check offsetField > 16 bits
-            System.out.println("error offsetField more than 16 bit");
-            isRun = false;
-        }
-
-        String offsetField = Integer.toBinaryString(test);          // ทำเป็น 16 บิต
-        if (test < 0) {
-            return offsetField.substring(16, 32);
-        } else {
-            for (int i = offsetField.length(); i < 16; i++) {
-                offsetField = "0" + offsetField;
-            }
-            return offsetField;
-        }
-    }
-
     private static String setReg(String field) {
         int test = Integer.valueOf(field);
         String temp = Integer.toBinaryString(convertStritoInt(field)); // convert decimal to binary
         if (test == 0 || test == 1) return "00" + temp;
         else if (test == 2 || test == 3) return "0" + temp;
         else return temp;
+    }
+
+    private static int convertStritoInt(String field) { // convert String to int
+        return Integer.parseInt(field); // return convert int
+    }
+
+    private static String setOffsetField(String field) {
+        int test = 0;
+        if (Pattern.matches(pattern, field)) {  //ถ้า field เป็นตัวเลข
+            test = Integer.valueOf(field);
+        }
+        else {
+            test = setVariable(field);          //ถ้า field เป็นตัวอักษร
+        }
+        if (test > 32767 || test < -32768) {                            //Check offsetField > 16 bits
+            System.out.println("error offsetField more than 16 bit");
+            isRun = false;
+        }
+
+        String offsetField = Integer.toBinaryString(test);          //แปลง address เป็น binary string
+
+        if (test < 0) {
+            return offsetField.substring(16, 32);                  // ตัดเอาเฉพาะ 16 bits negative values ของ offsetField เพราะ assembler
+        } else {
+            for (int i = offsetField.length(); i < 16; i++) {
+                offsetField = "0" + offsetField;
+            }
+
+            return offsetField;
+        }
+    }
+
+    private static int setVariable(String field) {
+        for (int i = 0; i < label.size(); i++) {
+            if (field.equals(label.get(i))) {
+
+                return valueOflabel.get(i); // โหลดค่าจาก label
+
+            }
+        }
+        //not find in label
+        System.out.println("does not exist variable " + field);
+        isRun = false;
+        return 0;
+
+    }
+
+    private static void print() throws IOException {
+        write(Integer.toString(result));
+        System.out.println(result);         //  แสดงผล
+
     }
 
 }
